c2bcf8fadfff87dc9ab759a207503638e3883f2e
Heta <est.fmapper@gmail.com>
2026-06-17 16:48:15 +0200

    Pressure

--- src/main/kotlin/fr/heta__h/squ_abyssal_bloom/event/pressure/PressureEffectEvent.kt
+++ src/main/kotlin/fr/heta__h/squ_abyssal_bloom/event/pressure/PressureEffectEvent.kt
@@ -33,8 +33,8 @@ object PressureEffectEvent {
     private const val PRESSURE_BASE_THRESHOLD = 45
     private const val PRESSURE_TIER_DEPTH = 15
     private const val PRESSURE_SEAL_BONUS = 15
-    private const val PRESSURE_DURATION = 400
-    private const val PRESSURE_DURATION_THRESHOLD = 300
+    private const val PRESSURE_DURATION = 500
+    private const val PRESSURE_DURATION_THRESHOLD = 400
 
     private const val INTERVAL_TIER_0 = 60L
     private const val INTERVAL_TIER_1 = 40L
